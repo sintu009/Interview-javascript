c8c8a0ad228a47c78a013da5aa3f7723b51c507e
SINTU KUMAR <topcurrentaffairs09@gmail.com>
2024-07-13 07:30:39 +0530

    first commit

---
+++ Day1.js
@@ -0,0 +1,41 @@
+// Activity 1 : variable declaration
+
+var x = 5;
+console.log(x)
+
+let y = "sintu";
+console.log(y)
+
+// Activity 2 : constant declaration
+
+
+const z = false;
+console.log(z)
+
+// Activity 3 : data type
+
+var x1 = 5;
+var x2 = "five"
+var x3 = 0;
+var x4 = {};
+var x5 = [];
+
+console.log(typeof (x1))
+console.log(typeof (x2))
+console.log(typeof (x3))
+console.log(typeof (x4))
+console.log(typeof (x5))
+
+// Activity 4 : Reassiging variable
+
+let x7 = "sintu";
+let x8 = "Kumar";
+
+console.log(x8)
+
+// Activity 5 : Understand const
+
+const x9 = "sintu";
+// const x9 = "kumar"
+
+// output: SyntaxError: Identifier 'x9' has already been declared
